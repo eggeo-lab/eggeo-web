34fe5b3ac2dc98762e79eb667daa269672bb77cd
eliasrivarola <elias.rivarola@teladochealth.com>
2026-04-14 01:03:38 -0300

    changes deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Esto es vital para que las rutas funcionen en www.eggeo.com.ar
+  base: "/eggeo-web/",
 });
